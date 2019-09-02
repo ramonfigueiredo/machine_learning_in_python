8727d85e3885ab3ffb44edb5237211a2c202859b
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2019-09-02 14:23:09 -0500

    Multiple Linear Regression

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Machine Learning in Python
 1. [Data Preprocessing](#data-preprocessing)
 2. [Regression](#regression)
 	1. [Simple Linear Regression](#simple-linear-regression)
+	2. [Multiple Linear Regression](#multiple-linear-regression)
 3. [How to run the Python program](#how-to-run-the-python-program)
 
 ## Data Preprocessing
@@ -33,6 +34,16 @@ a.  [data_preprocessing.py](https://github.com/ramonfigueiredopessoa/machine_lea
 
 a.  [simple_linear_regression.py](https://github.com/ramonfigueiredopessoa/machine_learning_in_python/blob/master/src/2_regression/1_simple_linear_regression/simple_linear_regression.py)
 
+### Multiple Linear Regression
+
+* Importing the dataset (50_Startups.csv)
+* Encoding categorical data
+* Avoiding the Dummy Variable Trap
+* Splitting the dataset into the Training set and Test set
+* Fitting Multiple Linear Regression to the Training set
+* Predicting the Test set results
+
+a.  [multiple_linear_regression.py](https://github.com/ramonfigueiredopessoa/machine_learning_in_python/blob/master/src/2_regression/2_multiple_linear_regression/multiple_linear_regression.py)
 
 ## How to run the Python program?
 
---
+++ src/2_regression/2_multiple_linear_regression/multiple_linear_regression.py
@@ -0,0 +1,43 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+ct = ColumnTransformer(
+    [('one_hot_encoder', OneHotEncoder(categories='auto'), [3])],    # The column numbers to be transformed (here is [3] but can be [0, 1, 2, 3, ..])
+    remainder='passthrough'                         				 # Leave the rest of the columns untouched
+)
+X = np.array(ct.fit_transform(X), dtype=np.float)
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+'''
+Linear regression is going to take care of feature scaling for us.
+Therefore we don't need to do feature scaling.
+'''
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression() 
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
