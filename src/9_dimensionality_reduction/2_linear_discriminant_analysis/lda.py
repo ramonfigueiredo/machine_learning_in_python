abffff5546b0803e1926b3303ac1a092955a93bb
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2019-10-10 10:27:46 -0500

    Linear Discriminant Analysis and Kernal PCA

--- src/9_dimensionality_reduction/2_linear_discriminant_analysis/lda.py
+++ src/9_dimensionality_reduction/2_linear_discriminant_analysis/lda.py
@@ -33,10 +33,12 @@ classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+print("Predicting the Test set results\n", y_pred)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix\n", cm)
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
